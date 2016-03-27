7492c056e468e0930978b02b9e1ff3976aa6081a
Gordon Zhang <gordonz@umich.edu>
2016-03-26 23:25:36 -0400

    Waypoint selection with mouse or controller

---
+++ Assets/ControlsDemo/Controls.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+public class Controls : MonoBehaviour  {
+    public enum Mode { Controller = 0, MouseKeyboard, Both }
+
+    [Header("Config")]
+    public int playerNum;
+    public Mode ControlsMode = Mode.Both;
+
+    private InputDevice inputDevice = null;
+
+    #region Public Properties
+    public bool isActive
+    {
+        get { return inputDevice != null; }
+    }
+
+    public InputDevice ID
+    {
+        get { return inputDevice; }
+    }
+    #endregion
+
+    #region Internal Methods
+    InputDevice UpdateState()
+    {
+        return (InputManager.Devices.Count > playerNum) ?
+                InputManager.Devices[playerNum] : null;
+    }
+
+    void Awake()
+    {
+        inputDevice = UpdateState();
+    }
+
+    void FixedUpdate()
+    {
+        inputDevice = UpdateState();
+    }
+    #endregion
+
+
+    #region Controls Properties + Methods
+    public Vector2 MoveDir(Vector3 origin, float maxDistance)
+    {
+        Vector2 ret = Vector2.zero;
+        if(ControlsMode == Mode.MouseKeyboard || ControlsMode == Mode.Both)
+        {
+            Vector3 mousePos2D = Input.mousePosition;
+            mousePos2D.z = -Camera.main.transform.position.z;
+            Vector3 mousePos3d = Camera.main.ScreenToWorldPoint(mousePos2D);
+            Vector3 dir3 = mousePos3d - origin;
+            Vector2 dir = new Vector2(dir3.x, dir3.y);
+            ret = Vector2.ClampMagnitude(dir, maxDistance);
+        }
+        if(ControlsMode == Mode.Controller || ControlsMode == Mode.Both)
+        {
+            Vector2 dir = Vector2.ClampMagnitude(ID.LeftStick, 1f);
+            ret = dir != Vector2.zero ? dir * maxDistance : ret;
+        }
+
+        return ret;
+    }
+
+    public Vector2 MoveDir(Vector2 origin, float maxDistance)
+    {
+        return MoveDir(new Vector3(origin.x, origin.y, 0f), maxDistance);
+    }
+
+    public bool SetPoint
+    {
+        get
+        {
+            bool ret = false;
+            if(ControlsMode == Mode.MouseKeyboard && Input.GetMouseButtonDown(0))
+            {
+                ret = true;
+            }
+            if (ControlsMode == Mode.Controller && ID.Action1.WasPressed)
+            {
+                ret = true;
+            }
+            if(ControlsMode == Mode.Both && (Input.GetMouseButtonDown(0) || ID.Action1.WasPressed))
+            {
+                ret = true;
+            }
+            return ret;
+        }
+    }
+
+    public bool UnsetPoint
+    {
+        get
+        {
+            bool ret = false;
+            if (ControlsMode == Mode.MouseKeyboard && Input.GetMouseButtonDown(1))
+            {
+                ret = true;
+            }
+            if (ControlsMode == Mode.Controller && ID.Action2.WasPressed)
+            {
+                ret = true;
+            }
+            if (ControlsMode == Mode.Both && (Input.GetMouseButtonDown(1) || ID.Action2.WasPressed))
+            {
+                ret = true;
+            }
+            return ret;
+        }
+    }
+
+    #endregion
+
+}
---
+++ Assets/ControlsDemo/MovementControls.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(Controls))]
+public class MovementControls : MonoBehaviour {
+
+    private Controls controls;
+
+    public float maxDistance;
+    public GameObject marker;
+
+    public Vector2 dir;
+    public List<Vector3> positions = new List<Vector3>();
+    public List<Vector3> directions = new List<Vector3>();
+    public List<GameObject> markers = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+        controls = GetComponent<Controls>();
+        positions.Add(transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(positions.Count < 1)
+        {
+            positions.Add(transform.position);
+        }
+        if(markers.Count < 1)
+        {
+            markers.Add(Instantiate(marker));
+        }
+
+        if(positions.Count > 1 && directions.Count > 0)
+        {
+            for(int i = 0; i < Mathf.Min(positions.Count, directions.Count); i++)
+            {
+                Debug.DrawRay(positions[i], directions[i], Color.red);
+            }
+        }
+
+        dir = controls.MoveDir(positions[positions.Count - 1], maxDistance);
+        Debug.DrawRay(positions[positions.Count - 1], new Vector3(dir.x, dir.y, 0f), Color.red);
+        markers[markers.Count - 1].transform.position = positions[positions.Count - 1] + new Vector3(dir.x, dir.y, 0f);
+
+        if (controls.SetPoint)
+        {
+            directions.Add(new Vector3(dir.x, dir.y, 0f));
+            positions.Add(positions[positions.Count - 1] + directions[positions.Count - 1]);
+            markers.Add(Instantiate(marker));
+        }
+
+        if(controls.UnsetPoint)
+        {
+            if(directions.Count > 0 && positions.Count > 1)
+            {
+                directions.RemoveAt(directions.Count - 1);
+                positions.RemoveAt(positions.Count - 1);
+                Destroy(markers[markers.Count - 1]);
+                markers.RemoveAt(markers.Count - 1);
+            }
+        }
+	}
+}
