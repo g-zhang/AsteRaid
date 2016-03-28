c926e2c5f324fc652e93a07f749e5793218022b3
Gordon Zhang <gordonz@umich.edu>
2016-03-27 23:40:36 -0400

    Fixed frame controls & Added maxdist per frame

--- Assets/ControlsDemo/Controls.cs
+++ Assets/ControlsDemo/Controls.cs
@@ -55,7 +55,7 @@ public class Controls : MonoBehaviour  {
             Vector2 dir = new Vector2(dir3.x, dir3.y);
             ret = Vector2.ClampMagnitude(dir, maxDistance);
         }
-        if(ControlsMode == Mode.Controller || ControlsMode == Mode.Both)
+        if(isActive && (ControlsMode == Mode.Controller || ControlsMode == Mode.Both))
         {
             Vector2 dir = Vector2.ClampMagnitude(ID.LeftStick, 1f);
             ret = dir != Vector2.zero ? dir * maxDistance : ret;
@@ -78,11 +78,11 @@ public class Controls : MonoBehaviour  {
             {
                 ret = true;
             }
-            if (ControlsMode == Mode.Controller && ID.Action1.WasPressed)
+            if (ControlsMode == Mode.Controller && isActive && ID.Action1.WasPressed)
             {
                 ret = true;
             }
-            if(ControlsMode == Mode.Both && (Input.GetMouseButtonDown(0) || ID.Action1.WasPressed))
+            if(ControlsMode == Mode.Both && (Input.GetMouseButtonDown(0) || (isActive && ID.Action1.WasPressed)))
             {
                 ret = true;
             }
@@ -99,11 +99,11 @@ public class Controls : MonoBehaviour  {
             {
                 ret = true;
             }
-            if (ControlsMode == Mode.Controller && ID.Action2.WasPressed)
+            if (ControlsMode == Mode.Controller && isActive && ID.Action2.WasPressed)
             {
                 ret = true;
             }
-            if (ControlsMode == Mode.Both && (Input.GetMouseButtonDown(1) || ID.Action2.WasPressed))
+            if (ControlsMode == Mode.Both && (Input.GetMouseButtonDown(1) || (isActive && ID.Action2.WasPressed)))
             {
                 ret = true;
             }
@@ -111,6 +111,47 @@ public class Controls : MonoBehaviour  {
         }
     }
 
+    public bool SetFrame
+    {
+        get
+        {
+            bool ret = false;
+            if (ControlsMode == Mode.MouseKeyboard && Input.GetKeyDown(KeyCode.E))
+            {
+                ret = true;
+            }
+            if (ControlsMode == Mode.Controller && isActive && ID.Action3.WasPressed)
+            {
+                ret = true;
+            }
+            if (ControlsMode == Mode.Both && (Input.GetKeyDown(KeyCode.E) || (isActive && ID.Action3.WasPressed)))
+            {
+                ret = true;
+            }
+            return ret;
+        }
+    }
+
+    public bool ActivateFrames
+    {
+        get
+        {
+            bool ret = false;
+            if (ControlsMode == Mode.MouseKeyboard && Input.GetKeyDown(KeyCode.Space))
+            {
+                ret = true;
+            }
+            if (ControlsMode == Mode.Controller && isActive && ID.Action4.WasPressed)
+            {
+                ret = true;
+            }
+            if (ControlsMode == Mode.Both && (Input.GetKeyDown(KeyCode.Space) || (isActive && ID.Action4.WasPressed)))
+            {
+                ret = true;
+            }
+            return ret;
+        }
+    }
     #endregion
 
 }
