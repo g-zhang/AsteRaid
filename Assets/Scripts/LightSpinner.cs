ae86ad3a698bc9a1c67bac1303ff8c6a222a286c
Jonathan Smith <jnthsmth@umich.edu>
2016-03-31 15:53:51 -0400

    day/night added

---
+++ Assets/Scripts/LightSpinner.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightSpinner : MonoBehaviour {
+
+	public float spinSpeed = 1f;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(spinSpeed * Time.deltaTime, 0f, 0f);
+	}
+}
