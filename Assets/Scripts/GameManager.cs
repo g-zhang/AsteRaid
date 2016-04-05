041100a320894fa80852ad5d86da1c454bbd0ac6
Ann Ho <kjuri27@yahoo.com>
2016-04-04 22:14:57 -0400

    Win state 80% there

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public enum Team
 {
@@ -11,4 +12,35 @@ public class GameManager : MonoBehaviour
 {
 	[Header("GameManager: Inspector Set Fields")]
 	public bool useInvulnTime = false;
+	public GameObject base_team1, base_team2;
+	public Canvas UICanvas;
+	bool win;
+
+	void Start() {
+		win = false;
+	}
+
+	void Update() {
+		winManager ();
+	}
+
+	void winManager() {
+		if ((base_team1 == null || base_team2 == null) && win == false) {
+			// pause game
+			Time.timeScale = 0;
+
+			// Show WIN UI element
+			if (base_team1 == null) {
+				UICanvas.GetComponent<UpdateUI>().displayWin(2);
+			} else if (base_team2 == null) {
+				UICanvas.GetComponent<UpdateUI>().displayWin(1);
+			}
+
+			win = true;
+		}
+
+		if (win) {
+			// If any player presses start, refresh scene
+		}
+	}
 }
--- Assets/Scripts/UpdateUI.cs
+++ Assets/Scripts/UpdateUI.cs
@@ -4,22 +4,25 @@ using UnityEngine.UI;
 
 public class UpdateUI : MonoBehaviour {
 
+	/*
     public GameObject Player0;
     public GameObject Player1;
     public GameObject Player2;
     public GameObject Player3;
+	*/
 
     public GameObject Base1;
     public GameObject Base2;
 
+	/*
     public Text Player0Text;
     public Text Player1Text;
     public Text Player2Text;
     public Text Player3Text;
+	*/
 
     public Text Base1Text;
     public Text Base2Text;
-
 
     // Use this for initialization
     void Start () {
@@ -28,12 +31,21 @@ public class UpdateUI : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		/*
         Player0Text.text = "P1 HP: " + Player0.GetComponent<HealthSystem>().currHealth;
         Player1Text.text = "P2 HP: " + Player1.GetComponent<HealthSystem>().currHealth;
         Player2Text.text = "P3 HP: " + Player2.GetComponent<HealthSystem>().currHealth;
         Player3Text.text = "P4 HP: " + Player3.GetComponent<HealthSystem>().currHealth;
+		*/
 
-        Base1Text.text = "Blue Base HP: " + Base1.GetComponent<HealthSystem>().currHealth;
-        Base2Text.text = "Red Base HP: " + Base2.GetComponent<HealthSystem>().currHealth;
+        Base1Text.text = "Blue Base HP: " + Base1.GetComponent<BaseHealth>().currHealth;
+        Base2Text.text = "Red Base HP: " + Base2.GetComponent<BaseHealth>().currHealth;
     }
+
+	public void displayWin(int winningTeam) {
+		GameObject wintext = transform.Find ("WinText").gameObject;
+		wintext.SetActive (true);
+
+		wintext.GetComponent<Text>().text = "Team " + winningTeam + " WINS";
+	}
 }
