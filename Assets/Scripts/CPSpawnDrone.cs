4b39bfc7fd6eca20d2f602150ef356112c092531
Ann Ho <kjuri27@yahoo.com>
2016-04-04 20:44:39 -0400

    Defense Drones done

--- Assets/Scripts/CPSpawnDrone.cs
+++ Assets/Scripts/CPSpawnDrone.cs
@@ -6,6 +6,7 @@ public class CPSpawnDrone : MonoBehaviour {
 
 	[Header("Inspector Set Fields")]
 	public GameObject attackDrone;
+	public GameObject defenseDrone;
 	public float spawnRate;
 	public int maxDrones;
 
@@ -16,8 +17,10 @@ public class CPSpawnDrone : MonoBehaviour {
 
 	[Header("Dynamically Set Fields")]
 	ControlPoint CP;
-	public List<GameObject> spawnedDrones_Team1;
-	public List<GameObject> spawnedDrones_Team2;
+	public List<GameObject> spawnedADrones_Team1;
+	public List<GameObject> spawnedADrones_Team2;
+	public List<GameObject> spawnedDDrones_Team1;
+	public List<GameObject> spawnedDDrones_Team2;
 
 	float elapsedSpawnDelay;
 
@@ -32,9 +35,14 @@ public class CPSpawnDrone : MonoBehaviour {
 
 			Team currTeam = whichTeam ();
 
-			if ((currTeam == Team.Team1 && (spawnedDrones_Team1.Count) < maxDrones) 
-				|| (currTeam == Team.Team2 && (spawnedDrones_Team2.Count) < maxDrones)) {
+			if ((currTeam == Team.Team1 && (spawnedADrones_Team1.Count) < maxDrones) 
+				|| (currTeam == Team.Team2 && (spawnedADrones_Team2.Count) < maxDrones)) {
 				spawnAttackDrone (currTeam);
+			}
+
+			if ((currTeam == Team.Team1 && (spawnedDDrones_Team1.Count) < maxDrones) 
+				|| (currTeam == Team.Team2 && (spawnedDDrones_Team2.Count) < maxDrones)) {
+				spawnDefenseDrone (currTeam);
 			}
 		}
 	}
@@ -60,12 +68,25 @@ public class CPSpawnDrone : MonoBehaviour {
 
 		if (teamNum == Team.Team1) {
 			aDrone.GetComponent<AttackDroneController> ().enemyBase = team2Base;
-			spawnedDrones_Team1.Add (aDrone);
+			spawnedADrones_Team1.Add (aDrone);
 		}
 		else if (teamNum == Team.Team2) {
 			aDrone.GetComponent<AttackDroneController> ().enemyBase = team1Base;
-			spawnedDrones_Team2.Add (aDrone);
+			spawnedADrones_Team2.Add (aDrone);
 		}
 	}
 
+	void spawnDefenseDrone(Team teamNum) {
+		GameObject dDrone = Instantiate (defenseDrone);
+		dDrone.GetComponent<DefenseDroneController> ().teamNumber = teamNum;
+		dDrone.GetComponent<DefenseDroneController> ().CPSpawn = this.gameObject;
+		dDrone.transform.position = transform.position;
+
+		if (teamNum == Team.Team1) {
+			spawnedDDrones_Team1.Add (dDrone);
+		}
+		else if (teamNum == Team.Team2) {
+			spawnedDDrones_Team2.Add (dDrone);
+		}
+	}
 }
