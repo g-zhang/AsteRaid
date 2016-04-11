46da994a7a825401dac1b48b3415509c4e1f8e23
Gordon Zhang <gordonz@umich.edu>
2016-04-11 01:53:17 -0400

    bullet trails now have team colors

--- Assets/Scripts/Weapon_Bullet.cs
+++ Assets/Scripts/Weapon_Bullet.cs
@@ -5,18 +5,30 @@ public class Weapon_Bullet : Weapon
 {
 	[Header("Weapon_Bullet: Inspector Set Fields")]
 	public float speed = 5f;
+    public float lingerTime = .5f;
 
 	[Header("Weapon_Bullet: Dynamically Set Fields")]
 	public Rigidbody rigid;
 	public Vector3 startPosition;
+    public float currLingerTime = 0f;
+    public bool isDead = false;
 
 	void Start()
 	{
 		rigid = GetComponent<Rigidbody>();
 		startPosition = transform.position;
 		rigid.velocity = startingVelocity * speed;
+        currLingerTime = lingerTime;
 
         GetComponent<Renderer>().material.color = GameManager.GM.teamColors[(int)teamNumber];
+
+        TrailRenderer trail = GetComponent<TrailRenderer>();
+        if (trail != null)
+        {
+            Color teamColor = GameManager.GM.teamColors[(int)teamNumber];
+            //trail.material.SetColor("_Color", new Color(teamColor.r, teamColor.b, teamColor.g, 1f));
+            trail.material.SetColor("_EmissionColor", teamColor);
+        }
 
 		return;
 	}
@@ -25,8 +37,21 @@ public class Weapon_Bullet : Weapon
 	{
 		if (Vector3.Magnitude(transform.position - startPosition) > maxDistance)
 		{
-			Destroy(gameObject);
+            isDead = true;
 		}
+
+        if(isDead)
+        {
+            GetComponent<MeshRenderer>().enabled = false;
+            GetComponent<Collider>().enabled = false;
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+            currLingerTime -= Time.deltaTime;
+
+            if(currLingerTime <= 0f)
+            {
+                Destroy(gameObject);
+            }
+        }
 		return;
 	}
 
@@ -61,7 +86,7 @@ public class Weapon_Bullet : Weapon
 			}
 		}
 
-		Destroy(gameObject);
+        isDead = true;
 		return;
 	}
 }
