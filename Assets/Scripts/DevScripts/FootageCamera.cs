b1dd415e9d92e6859170583d20264017b92840a3
Ann Ho <kjuri27@yahoo.com>
2016-04-14 06:29:00 -0400

    My camera scene got wiped out, but I'm throwing in the really basic script for footage camera controls
    Don’t use for regular stuff lol

---
+++ Assets/Scripts/DevScripts/FootageCamera.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FootageCamera : MonoBehaviour {
+
+	public float cameraSpeed;
+
+	// Camera controls are i,j,k,l and are similar to wasd
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.I)) {
+			transform.position += transform.forward * cameraSpeed;
+		}
+		if (Input.GetKey (KeyCode.K)) {
+			transform.position -= transform.forward * cameraSpeed;
+		}
+		if (Input.GetKey (KeyCode.J)) {
+			transform.position -= transform.right * cameraSpeed;
+		}
+		if (Input.GetKey (KeyCode.L)) {
+			transform.position += transform.right * cameraSpeed;
+		}
+	}
+}
