c4d7132933aa16d99668f1f67ee7ad65a8662e80
Gordon Zhang <gordonz@umich.edu>
2016-04-17 04:12:56 -0400

    removed unused var

--- Assets/Scripts/ShipColor.cs
+++ Assets/Scripts/ShipColor.cs
@@ -2,36 +2,38 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class ShipColor : MonoBehaviour {
+public class ShipColor : MonoBehaviour
+{
 
     List<Color> original;
     List<Material> mats;
 
     bool isChanging = false;
     float currColorTime = 0f;
-    Color color = Color.clear;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         mats = new List<Material>(GetComponent<Renderer>().materials);
         original = new List<Color>(mats.Count);
-        foreach(Material mat in mats)
+        foreach (Material mat in mats)
         {
             original.Add(mat.color);
         }
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         if (currColorTime > 0f)
         {
             currColorTime -= Time.deltaTime;
         }
-        else if(isChanging)
+        else if (isChanging)
         {
             isChanging = false;
             currColorTime = 0f;
-            for(int i = 0; i < mats.Count; i++)
+            for (int i = 0; i < mats.Count; i++)
             {
                 mats[i].color = original[i];
             }
