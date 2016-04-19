77aa221463a7bd22191646cac06a8b00d248877b
Jonathan Smith <jnthsmth@umich.edu>
2016-04-19 01:19:39 -0400

    fixing boost in death

--- Assets/Scripts/IndividualCanvas.cs
+++ Assets/Scripts/IndividualCanvas.cs
@@ -1,7 +1,20 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class IndividualCanvas : MonoBehaviour {
+
+
+	public Player player;
+	public GameObject go;
+	public ShipControls shipControls;
+
+	public Image RBIcon;
+	public Image Bomb1Icon;
+	public Image Bomb2Icon;
+	public Image Bomb3Icon;
+	public Slider UltSlider;
+	public Slider BoostSlider;
 
 	// Use this for initialization
 	void Start () {
@@ -10,6 +23,10 @@ public class IndividualCanvas : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+		Bomb3Icon.enabled = (player.grenadeAmmo >= 3);
+		Bomb2Icon.enabled = (player.grenadeAmmo >= 2);
+		Bomb1Icon.enabled = (player.grenadeAmmo >= 1);
+
 	}
 }
