bf96fd77f70aed4952d99f94bb210fb1701e601b
Ann Ho <kjuri27@yahoo.com>
2016-04-18 23:25:10 -0400

    Tutorial Update but barely

--- Assets/Scripts/_Tutorial Scripts/TutorialManager.cs
+++ Assets/Scripts/_Tutorial Scripts/TutorialManager.cs
@@ -2,14 +2,29 @@
 using System.Collections;
 
 public class TutorialManager : MonoBehaviour {
+	public Canvas T1P1, T1P2, T2P1, T2P2;
+	public bool[] playerReady = new bool[GameManager.NUM_PLAYERS];
+	public GameObject[] players = new GameObject[GameManager.NUM_PLAYERS];
 
-	// Use this for initialization
-	void Start () {
-	
+	void Start() {
+		for (int i = 0; i < playerReady.Length; i++) {
+			playerReady [i] = false;
+		}
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-	
+		if (allReady ()) {
+			// do ready stuff
+			// move to next scene
+		}
+
+	}
+
+	bool allReady() {
+		for (int i = 0; i < playerReady.Length; i++) {
+			if (!playerReady [i])
+				return false;
+		}
+		return true;
 	}
 }
