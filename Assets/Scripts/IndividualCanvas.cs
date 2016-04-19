03729f6f4d4d53320181a3175e9211228232c0a6
Jonathan Smith <jnthsmth@umich.edu>
2016-04-19 00:56:57 -0400

    more spritely goodness

---
+++ Assets/Scripts/IndividualCanvas.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IndividualCanvas : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
