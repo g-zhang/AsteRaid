6aed95e4daac8c5ddd64ae758595f10967b04606
Ann Ho <kjuri27@yahoo.com>
2016-04-21 02:51:39 -0400

    Tutorial is done??

---
+++ Assets/Scripts/_Tutorial Scripts/AutoChargeUlt.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoChargeUlt : MonoBehaviour {
+	void OnTriggerEnter(Collider other) {
+		if (other.transform.parent.GetComponent<Player> () != null) {
+			other.transform.parent.GetComponent<Player> ().ultCharges = other.transform.parent.GetComponent<Player> ().chargesNeededForUlt;
+			Destroy (this.gameObject);
+		}
+	}
+}
