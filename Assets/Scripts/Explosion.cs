f1e94ac002f377992b540af67ce63ae34516e755
David A. Kolb <dakbox10@gmail.com>
2016-04-21 03:50:51 -0400

    Added a bunch of explosion prefabs.

---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+	ParticleSystem particles;
+
+	void Awake()
+	{
+		particles = transform.Find("Particle").GetComponent<ParticleSystem>();
+		return;
+	}
+
+	void Update()
+	{
+		if (!particles.isPlaying)
+		{
+			Destroy(gameObject);
+		}
+		return;
+	}
+}
